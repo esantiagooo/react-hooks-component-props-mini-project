02b764613c80d3e67fdaa34c506700c80f08542f
Eric <eric.yanez50@qmail.cuny.edu>
2023-01-06 16:02:44 -0500

    done

--- src/components/Article.js
+++ src/components/Article.js
@@ -8,3 +8,9 @@ const Article = ({title,date,preview}) => {
     )
 }
 export default Article
+
+// Props are data passed from a Parent component down to the child component.
+
+// That is why properties such as title, date  and preview are accesible in Article Component. It's Parent Component ArticleList retains the information and passes along the value as props. Since Article is a child of ArticleList.
+
+// think Russian doll but for information
